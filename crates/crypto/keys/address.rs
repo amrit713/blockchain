0c94a202385535bab9fabc6bfc36f655dff766d3
amrit-aozora <amrit.intp@aozora-okinawa.com>
2026-08-11 18:55:03 +0545

    created address  data type

---
+++ crates/crypto/keys/address.rs
@@ -0,0 +1,21 @@
+use crate::{PublicKey, keys::public_key};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Address(pub PublicKey);
+
+impl Address {
+    pub fn new(public_key: PublicKey) -> Self {
+        Self(public_key)
+    }
+
+    pub fn public_key(&self) -> &PublicKey {
+        &self.0
+    }
+}
+
+impl From<PublicKey> for Address {
+    fn from(public_key: PublicKey) -> Self {
+        Address(public_key)
+    }
+}
